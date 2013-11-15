47d3b15bf7c08f25f4998d261033cb53c4b9780e
bartvitaly <bartvitaly@gmail.com>
2013-11-15 09:31:01 +0200

    putCopy delete object added

---
+++ src/test/java/Api_02_PutCopy_Object_Test.java
@@ -0,0 +1,110 @@
+package test.java;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import me.s3for.common.Common;
+import me.s3for.common.S3Utils;
+import me.s3for.common.StringUtils;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+import com.amazonaws.services.s3.model.CopyObjectResult;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+
+public class Api_02_PutCopy_Object_Test extends TestInitialize {
+
+	S3Utils s3Utils, s3UtilsAws;
+	String fileName = "file.txt";
+	String fileNameNew = "file_new.txt";
+	File file, fileAws;
+	PutObjectResult putObjectResult, putObjectResultAws;
+	String[] avoidKeys = { "Last-Modified", "Keep-Alive" };
+
+	/**
+	 * @desc The code to be run before each test
+	 */
+
+	@BeforeGroups(groups = { "api" })
+	public void before() {
+		// initiate S3 and AWS
+		s3Utils = new S3Utils(keyS3, secretS3, serverS3);
+		s3UtilsAws = new S3Utils();
+
+		// set bucket to work with
+		s3Utils.setBacket(bucketName);
+		s3UtilsAws.setBacket(bucketNameAws);
+
+		// put a file in a basket
+		file = S3Utils.createSampleFile();
+		putObjectResult = s3Utils.put(fileName, file);
+		putObjectResultAws = s3UtilsAws.put(fileName, file);
+
+	}
+
+	/**
+	 * @desc Check ability to write to a bucket
+	 * 
+	 * @throws IOException
+	 */
+
+	@Test(groups = { "api" })
+	public void bucketWriteTest() throws Exception {
+
+		// Copy objects
+		CopyObjectResult copyObjectResult = s3Utils.copyObject(fileName,
+				fileNameNew);
+		CopyObjectResult copyObjectResultAws = s3UtilsAws.copyObject(fileName,
+				fileNameNew);
+
+		Assert.assertEquals(copyObjectResult.getETag(),
+				putObjectResult.getETag());
+		Assert.assertEquals(copyObjectResultAws.getETag(),
+				putObjectResultAws.getETag());
+
+		// Get S3 objects
+		S3Object s3Object = s3Utils.get(fileName);
+		S3Object s3ObjectNew = s3Utils.get(fileNameNew);
+		S3Object s3ObjectAws = s3UtilsAws.get(fileNameNew);
+
+		ObjectMetadata s3ObjectMetadata = s3Object.getObjectMetadata();
+		ObjectMetadata s3ObjectMetadataNew = s3ObjectNew.getObjectMetadata();
+		ObjectMetadata s3ObjectMetadataAws = s3ObjectAws.getObjectMetadata();
+
+		// Get file content
+		String content = StringUtils.inputStreamToString(s3Object
+				.getObjectContent());
+		String contentNew = StringUtils.inputStreamToString(s3ObjectNew
+				.getObjectContent());
+		String contentAws = StringUtils.inputStreamToString(s3ObjectAws
+				.getObjectContent());
+
+		Assert.assertEquals(content, contentAws);
+		Assert.assertEquals(content, contentNew);
+
+		Map<String, Object> map = Common.compareMaps(
+				s3ObjectMetadata.getRawMetadata(),
+				s3ObjectMetadataNew.getRawMetadata(), avoidKeys);
+
+		System.out.println("\nMetadata: S3 vs S3New");
+		Common.printMap(map);
+
+		Assert.assertTrue(map.size() == 0,
+				"Objects' metadata are not the same. S3 vs S3New");
+
+		map = Common.compareMaps(s3ObjectMetadata.getRawMetadata(),
+				s3ObjectMetadataAws.getRawMetadata(), avoidKeys);
+
+		System.out.println("Metadata: S3 vs AWS");
+		Common.printMap(map);
+
+		Assert.assertTrue(map.size() == 0,
+				"Objects' metadata are not the same. S3 vs AWS");
+
+	}
+}
---
+++ src/test/java/Api_03_Delete_Object_Test.java
@@ -0,0 +1,58 @@
+package test.java;
+
+import java.io.File;
+import java.io.IOException;
+
+import me.s3for.common.S3Utils;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+public class Api_03_Delete_Object_Test extends TestInitialize {
+
+	S3Utils s3Utils, s3UtilsAws;
+	String fileName = "file.txt";
+	File file, fileAws;
+	String[] avoidKeys = { "Last-Modified", "Keep-Alive" };
+
+	/**
+	 * @desc The code to be run before each test
+	 */
+
+	@BeforeGroups(groups = { "api" })
+	public void before() {
+		// initiate S3 and AWS
+		s3Utils = new S3Utils(keyS3, secretS3, serverS3);
+		s3UtilsAws = new S3Utils();
+
+		// set bucket to work with
+		s3Utils.setBacket(bucketName);
+		s3UtilsAws.setBacket(bucketNameAws);
+
+		// put a file in a basket
+		file = S3Utils.createSampleFile();
+		s3Utils.put(fileName, file);
+		s3UtilsAws.put(fileName, file);
+
+	}
+
+	/**
+	 * @desc Check ability to delete object from a bucket
+	 * 
+	 * @throws IOException
+	 */
+
+	@Test(groups = { "api" })
+	public void bucketWriteTest() throws Exception {
+
+		// Delete objects
+		s3Utils.deleteObject(fileName);
+		s3UtilsAws.deleteObject(fileName);
+
+		// Get S3 objects
+		Assert.assertNull(s3Utils.get(fileName), "Object was not deleted");
+		Assert.assertNull(s3UtilsAws.get(fileName), "Object was not deleted");
+
+	}
+}
